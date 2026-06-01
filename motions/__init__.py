56e16422f9c5d5fdba57f9767acad4069c22e047
tsingqingyun <962133115@qq.com>
2026-06-01 22:05:54 +0800

    update

--- motions/__init__.py
+++
@@ -1,3 +0,0 @@
-from .drive import LEFT, RIGHT, MotionController
-from .orbit import circle_cube
-from .spin import spin_in_place
--- motions/spin.py
+++
@@ -1,4 +0,0 @@
-def spin_in_place(motion, direction, seconds, duty_cycle=20):
-    """Self-rotation: turn the chassis around its own centre."""
-    motion.turn_in_place(direction, seconds, duty_cycle=duty_cycle)
-
